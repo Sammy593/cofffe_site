e571bc7e401f059b6aeddd4ad82d2cddd124a999
SammyEc <samuelto1359@gmail.com>
2023-08-31 23:58:47 -0500

    SW - almacenar y usar archivos de cache

--- src/services/sw1.js
+++ src/services/sw1.js
@@ -1,3 +1,47 @@
-self.addEventListener('fetch', e =>{
-     console.log(e);
- })
+let CACHE_NAME = 'my-cache';
+let urlsToCache = [
+    '../index.css',
+    '../assets/img/logos/coffee-transparente.png',
+    '../assets/js/app.js'
+
+    ];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+  /*
+  self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['pigment'];
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });*/
